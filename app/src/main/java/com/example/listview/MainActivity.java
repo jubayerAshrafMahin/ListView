3ebcabc85bc881c87ff1a06ec75047c336e32b24
jubayerAshrafMahin <mahinashraf991@gmail.com>
2024-12-25 11:51:19 +0600

    initial project

---
+++ app/src/main/java/com/example/listview/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.listview;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView wondersListView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        wondersListView = findViewById(R.id.ltv);
+
+        String[] wonders = getResources().getStringArray(R.array.wonders);
+
+        ArrayAdapter <String> adapter= new ArrayAdapter<>(this, R.layout.listview_layout, R.id.tv_wonders, wonders);
+        wondersListView.setAdapter(adapter);
+
+        wondersListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                String wonder = wonders[position];
+                Toast.makeText(MainActivity.this, wonder, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
